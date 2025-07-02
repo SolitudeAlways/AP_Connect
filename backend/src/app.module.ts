a1414829e8f28d10905d959405ee48e693e8ecc1
Миша Чугунов <mishachugunov@MacBook-Air-Misa.local>
2025-07-02 17:32:32 +0500

    Создание проекта и подключение к нему основных зависимостей

---
+++ backend/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: 'config.env' });
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT ?? '5432', 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // на проде лучше отключить и миграции использовать
+      autoLoadEntities: true,
+    }),
+  ],
+})
+export class AppModule {}
